8839ffaafc9039c6687e36bc6ac1e2eebb180cb1
Sofia Brazda <sofiabrazda@Sofias-MacBook-Pro-4.local>
2025-07-28 14:36:18 -0700

    Lets see if this changes anything

--- public/images/p5/pixel-note_2025_07_12_00_16_01/sketch.js
+++ public/images/p5/pixel-note_2025_07_12_00_16_01/sketch.js
@@ -1,3 +1,6 @@
+let showMessage = false;
+let messageAlpha = 0;
+
 let musicNote = [
   "00000000110000000000", 
 "00000001111000000000", 
@@ -31,17 +34,19 @@ let musicNote = [
 
 let pixelSize = 20;
 let offsetX = 100;
-let offsetY = 20;
+let offsetY = 50;
 let particles = [];
+let exploded = false;
+let messageShown = false;
 
 function setup() {
-  let canvas = createCanvas(400, 600); // adjust size if needed
-  canvas.parent("gallery-content"); // attaches canvas to your <div id="gallery-content">
+  createCanvas(600, 800);
   noStroke();
+  background('#1f2573');
 }
 
 function draw() {
-  background(255);
+  background('#1f2573');
 
   if (particles.length > 0) {
     for (let p of particles) {
@@ -49,34 +54,51 @@ function draw() {
       p.show();
     }
     particles = particles.filter(p => !p.isDead());
-  } else {
-    // Draw the music note normally
+
+    // if all particles gone, show message
+    if (particles.length === 0 && exploded) {
+      messageShown = true;
+    }
+
+  } else if (!exploded) {
+    // draw the original music note
     for (let y = 0; y < musicNote.length; y++) {
       for (let x = 0; x < musicNote[y].length; x++) {
         if (musicNote[y][x] === "1") {
-          fill(0);
+          fill('#db8ae2');
           rect(offsetX + x * pixelSize, offsetY + y * pixelSize, pixelSize, pixelSize);
         }
       }
     }
   }
+
+  if (messageShown) {
+  if (messageAlpha < 255) {
+    messageAlpha += 3; // adjust this value to control fade speed
+  }
+  fill(219, 138, 226, messageAlpha); // #333 is rgb(51,51,51)
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text("i'm glad you're here", width / 2, height / 2);
+}
 }
 
 // Particle class
 class Particle {
   constructor(x, y) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(1, 4));
+    this.vel = p5.Vector.random2D().mult(random(0.3, 1.2));
     this.lifetime = 255;
   }
 
   update() {
     this.pos.add(this.vel);
-    this.lifetime -= 4;
+    this.lifetime -= 2;
   }
 
   show() {
-    fill(0, this.lifetime);
+    fill('#db8ae2');
+    fill(color('#db8ae2').levels[0], color('#db8ae2').levels[1], color('#db8ae2').levels[2], this.lifetime);
     rect(this.pos.x, this.pos.y, pixelSize, pixelSize);
   }
 
@@ -85,15 +107,24 @@ class Particle {
   }
 }
 
-// When you click, explode all pixels
+// On click, explode the note
 function mousePressed() {
-  for (let y = 0; y < musicNote.length; y++) {
-    for (let x = 0; x < musicNote[y].length; x++) {
-      if (musicNote[y][x] === "1") {
-        let px = offsetX + x * pixelSize;
-        let py = offsetY + y * pixelSize;
-        particles.push(new Particle(px, py));
+  if (!exploded) {
+    for (let y = 0; y < musicNote.length; y++) {
+      for (let x = 0; x < musicNote[y].length; x++) {
+        if (musicNote[y][x] === "1") {
+          let px = offsetX + x * pixelSize;
+          let py = offsetY + y * pixelSize;
+          particles.push(new Particle(px, py));
+        }
       }
     }
+    exploded = true;
+  }
+}
+
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    saveCanvas('music-note', 'png');
   }
 }
